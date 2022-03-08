b8487c541a7baf2eb2135c609e0e94d3e142a55a
Nick <nikosfeel@gmail.com>
2022-03-08 20:04:47 +0200

    beercontroller

---
+++ BeverageProject/Controllers/BeerController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Entities.Products;
+using MyDatabase;
+
+namespace BeverageProject.Controllers
+{
+    public class BeerController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Beer
+        public ActionResult Index(string category)
+        {
+            var beers = db.Beers.Where(x=>x.Category.Kind == category).ToList();
+            return View(beers);
+        }
+
+        // GET: Beer/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Beer beer = db.Beers.Find(id);
+            if (beer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(beer);
+        }
+
+        // GET: Beer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Beer/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Description,Price,PhotoUrl")] Beer beer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Beers.Add(beer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(beer);
+        }
+
+        // GET: Beer/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Beer beer = db.Beers.Find(id);
+            if (beer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(beer);
+        }
+
+        // POST: Beer/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description,Price,PhotoUrl")] Beer beer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(beer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(beer);
+        }
+
+        // GET: Beer/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Beer beer = db.Beers.Find(id);
+            if (beer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(beer);
+        }
+
+        // POST: Beer/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Beer beer = db.Beers.Find(id);
+            db.Beers.Remove(beer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
