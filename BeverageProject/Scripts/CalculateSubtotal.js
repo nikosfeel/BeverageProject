6f9048a1755a6a43acfed69745dd5a883a679d47
Nick <nikosfeel@gmail.com>
2022-03-11 00:51:38 +0200

    Calculate SubTotal Final

--- BeverageProject/Scripts/CalculateSubtotal.js
+++ BeverageProject/Scripts/CalculateSubtotal.js
@@ -1,10 +1,10 @@
 ﻿let subtotal = document.getElementById("subtotal")
 let price = parseFloat(document.getElementById("price").innerText)
 let quantity = document.getElementById("quantity")
-subtotal.innerText = price
+subtotal.innerText = price + "€"
 
 quantity.addEventListener("input", updateValue);
 
 function updateValue(e) {
-    subtotal.innerText = parseFloat(e.data) * price
+    subtotal.innerText = parseFloat(e.data) * price + "€"
 }
