50145e5ec156220a8a86046fc20602adeb203460
Natalia Papadatou <papadnatalia@gmail.com>
2022-03-15 21:50:38 +0200

    Admin

--- BeverageProject/Scripts/CalculateSubtotal.js
+++ BeverageProject/Scripts/CalculateSubtotal.js
@@ -1,11 +1,11 @@
-﻿let subtotal = document.getElementById("subtotal")
-let price = parseFloat(document.getElementById("price").innerText)
-let quantity = document.getElementById("quantity")
-subtotal.innerText = price + "€"
+﻿let subtotal = document.getElementById("subtotal");
+let price = parseFloat(document.getElementById("price").innerText);
+let quantity = document.getElementById("quantity");
+subtotal.innerText = price + "€";
 
 quantity.addEventListener("input", updateValue);
 
 function updateValue(e) {
     
-    subtotal.innerText = parseFloat(e.data) * price + "€";
+    subtotal.innerText += parseFloat(e.data) * price.toFixed(2) + "€";
 }
