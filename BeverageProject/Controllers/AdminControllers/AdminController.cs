29b0ac42cd52783ade2fd36e225f2782d2a2cf74
Christos Dimitrakopoulos <christos.dimitrakopoulos@outlook.com>
2022-03-17 14:39:44 +0200

    AdminPage Completed

--- BeverageProject/Controllers/AdminControllers/AdminController.cs
+++ BeverageProject/Controllers/AdminControllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyDatabase;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,7 +9,9 @@ namespace BeverageProject.Controllers.AdminControllers
 {
     public class AdminController : Controller
     {
-        // GET: Admin
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        //----------Side Bar Start----------
         public ActionResult Dashboard()
         {
             return View();
@@ -17,15 +20,31 @@ namespace BeverageProject.Controllers.AdminControllers
         {
             return View();
         }
+        public ActionResult CustomerInvoice()
+        {
+            return View();
+        }
+        public ActionResult PrintCustomerInvoice()
+        {
+            return View();
+        }
         public ActionResult AllProducts()
         {
             return View();
         }
-        public ActionResult LockScreen()
+        public ActionResult AllBeers()
         {
             return View();
         }
-        public ActionResult UserProfile()
+        public ActionResult AllSpirits()
+        {
+            return View();
+        }
+        public ActionResult AllWhiskeys()
+        {
+            return View();
+        }
+        public ActionResult AllWines()
         {
             return View();
         }
@@ -33,5 +52,33 @@ namespace BeverageProject.Controllers.AdminControllers
         {
             return View();
         }
+        public ActionResult ComposeMail()
+        {
+            return View();
+        }
+        public ActionResult ViewMail()
+        {
+            return View();
+        }
+        public ActionResult LockScreen()
+        {
+            return View();
+        }
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        public ActionResult LoginPage()
+        {
+            return View();
+        }
+        //----------Side Bar End----------
+
+        //----------Side Bar Start----------
+        public ActionResult UserProfile()
+        {
+            return View();
+        }
+        //----------Side Bar End----------
     }
 }
