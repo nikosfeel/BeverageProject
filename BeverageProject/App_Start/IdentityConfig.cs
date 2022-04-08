0cb7bc69543a16759d2fbef851c06ab8c48f3c24
sofianna12 <sofianna02@gmail.com>
2022-04-08 07:59:19 +0300

    two-factor authentication

--- BeverageProject/App_Start/IdentityConfig.cs
+++ BeverageProject/App_Start/IdentityConfig.cs
@@ -13,6 +13,11 @@ using Microsoft.Owin.Security;
 using BeverageProject.Models;
 using Entities.IdentityUsers;
 using MyDatabase;
+using System.Configuration;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio;
+using Twilio.Types;
+using System.Diagnostics;
 
 namespace BeverageProject
 {
@@ -20,8 +25,36 @@ namespace BeverageProject
     {
         public Task SendAsync(IdentityMessage message)
         {
-            // Plug in your email service here to send an email.
+            // Twilio Begin
+            var accountSid = ConfigurationManager.AppSettings["SMSAccountIdentification"];
+            var authToken = ConfigurationManager.AppSettings["SMSAccountPassword"];
+            var fromNumber = ConfigurationManager.AppSettings["SMSAccountFrom"];
+
+            TwilioClient.Init(accountSid, authToken);
+
+            MessageResource result = MessageResource.Create(
+            new PhoneNumber(message.Destination),
+            from: new PhoneNumber(fromNumber),
+            body: message.Body
+            );
+
+            ////Status is one of Queued, Sending, Sent, Failed or null if the number is not valid
+            Trace.TraceInformation(result.Status.ToString());
+            //Twilio doesn't currently have an async API, so return success.
             return Task.FromResult(0);
+            //Twilio End
+
+            // ASPSMS Begin 
+            // var soapSms = new MvcPWx.ASPSMSX2.ASPSMSX2SoapClient("ASPSMSX2Soap");
+            // soapSms.SendSimpleTextSMS(
+            //   System.Configuration.ConfigurationManager.AppSettings["SMSAccountIdentification"],
+            //   System.Configuration.ConfigurationManager.AppSettings["SMSAccountPassword"],
+            //   message.Destination,
+            //   System.Configuration.ConfigurationManager.AppSettings["SMSAccountFrom"],
+            //   message.Body);
+            // soapSms.Close();
+            return Task.FromResult(0);
+            // ASPSMS End
         }
     }
 
--- BeverageProject/Models/ManageViewModels.cs
+++ BeverageProject/Models/ManageViewModels.cs
@@ -7,6 +7,7 @@ namespace BeverageProject.Models
 {
     public class IndexViewModel
     {
+        
         public bool HasPassword { get; set; }
         public IList<UserLoginInfo> Logins { get; set; }
         public string PhoneNumber { get; set; }
